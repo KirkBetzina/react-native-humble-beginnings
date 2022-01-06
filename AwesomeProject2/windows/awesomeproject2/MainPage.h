fd3bd21c564197bc157d0273b6ff1df910e9e217
kirk <kirk.betzina@gmail.com>
2022-01-06 17:37:16 -0500

    first commit hopefully in the correct place for repository

---
+++ AwesomeProject2/windows/awesomeproject2/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::awesomeproject2::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::awesomeproject2::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
